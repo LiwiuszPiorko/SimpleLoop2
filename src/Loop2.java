7b9ab920950c697d6e722f870fc7581fb3d24b30
Liwiusz Piórko <liwiuszpiorko@wp.pl>
2018-09-25 12:59:38 +0200

    Pierwszy etap

---
+++ src/Loop2.java
@@ -0,0 +1,20 @@
+public class Loop2 {
+
+    public static void main(String[] args) {
+
+        int[] tablica = new int[100];
+        for (int i = 0; i <= tablica.length; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+
+            }
+
+        }
+    }
+
+}
+
+
+
+
+
