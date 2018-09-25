a6f04d34e7350f33ece3175f85c80876a14de710
Liwiusz Piórko <liwiuszpiorko@wp.pl>
2018-09-25 16:08:03 +0200

    Ostateczna wersja

--- src/Loop2.java
+++ src/Loop2.java
@@ -2,17 +2,22 @@ public class Loop2 {
 
     public static void main(String[] args) {
 
-        int[] tablica = new int[100];
-        for (int i = 0; i <= tablica.length; i++) {
+        int suma = 0;
+        for (int i = 0; i < 101; i++) {
             if (i % 2 == 0) {
+                suma = suma + i;
                 System.out.println(i);
-
             }
-
         }
+        System.out.println(suma);
     }
-
 }
+
+
+
+
+
+
 
 
 
